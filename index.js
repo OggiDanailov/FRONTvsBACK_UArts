ab78db013bfa3e2020338ae0d5c55bf90e4493ef
OggiDanailov <ogidan@abv.bg>
2019-03-22 14:05:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var submit = document.getElementsByClassName('submit-js')[0]
+
+
+
+setInterval(function(){
+	if(submit.style.backgroundColor == 'black'){
+		submit.style.color = 'black'
+		submit.style.backgroundColor = 'red'		
+		submit.style.fontSize = "8px"
+	} else {
+		submit.style.color = 'white'
+		submit.style.backgroundColor = 'black'
+		submit.style.fontSize = "6px"
+	}
+}, 1000)
