99bbdc283c1c01e3bba64749c1b4aa5e7dcdb194
OggiDanailov <ogidan@abv.bg>
2019-03-27 23:45:06 -0700

    db looping done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var submit = document.getElementsByClassName('submit-js')[0]
-
+var listNames = document.getElementsByClassName("list-names")
 
 
 setInterval(function(){
@@ -13,3 +13,28 @@ setInterval(function(){
 		submit.style.fontSize = "6px"
 	}
 }, 2000)
+
+	var i = 0;
+setInterval(function(){
+	if(i > listNames.length){
+		i = 0;
+		changeColor(i)		
+	}else {
+		changeColor(i)
+	}
+	i += 1;
+}, 1000)
+
+
+function changeColor(i){
+	console.log(i)
+	if(i == 0){
+		listNames[i].style.color = 'red'
+	}else if(i > 0 && i < listNames.length){
+		listNames[i].style.color = 'red'
+		listNames[i-1].style.color = 'black'
+	}else if(i == listNames.length){
+		listNames[i-1].style.color='black'
+	}
+}
+
