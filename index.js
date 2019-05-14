397c84c9623505bf4d1048b0ac99f6f255301a13
Oggi Danailov <oxd017@jefferson.edu>
2019-05-14 12:24:34 -0400

    renaming before meeting may 14

--- index.js
+++ index.js
@@ -1,41 +1,67 @@
-var submit = document.getElementsByClassName('submit-js')[0]
-var listNames = document.getElementsByClassName("list-names")
+var input = document.getElementById("input-example")
+var p = document.getElementById("p-example")
+var div = document.getElementById("div-example")
+var h3 = document.getElementById("h3-example")
+
+var pDestination = document.getElementById("p-destination")
+var inputDestination = document.getElementById("input-destination")
+var divDestination = document.getElementById("div-destination")
+var h3Destination = document.getElementById("h3-destination")
+
+input.addEventListener('click', function(){
+	inputDestination.style.visibility = 'visible'
+})
+
+div.addEventListener("click", function(){
+	divDestination.style.visibility = 'visible'
+})
+
+p.addEventListener('click', function(){
+	pDestination.style.visibility = 'visible'
+})
+
+h3.addEventListener('click', function(){
+	h3Destination.style.visibility = 'visible'
+})
+
+
+var cssInput = document.getElementById("css-input-example")
+var cssDiv = document.getElementById("css-div-example")
+var cssP = document.getElementById("css-p-example")
+var cssH3 = document.getElementById("css-h3-example")
+
+var inputDestinationCss = document.getElementById("input-destination-css") 
+var divDestinationCss = document.getElementById("div-destination-css") 
+var pDestinationCss = document.getElementById("p-destination-css") 
+var h3DestinationCss = document.getElementById("h3-destination-css") 
+
+cssInput.addEventListener('click', function(){
+	console.log('asdfasd')
+	inputDestinationCss.style.backgroundColor = 'lightgreen'
+})
+
+cssDiv.addEventListener('click', function(){
+	divDestinationCss.style.height = '100px'
+	divDestinationCss.style.backgroundColor = 'lightgreen'
+})
+
+cssP.addEventListener("click", function(){
+	pDestinationCss.style.marginLeft = '30px'
+	pDestinationCss.style.boxShadow = '4px 4px 4px grey'
+})
+
+cssH3.addEventListener('click', function(){
+	h3DestinationCss.style.fontSize = '30px'
+	h3DestinationCss.style.color = 'red'
+})
 
 
 
-setInterval(function(){
-	if(submit.style.backgroundColor == 'black'){
-		submit.style.color = 'black'
-		submit.style.backgroundColor = 'red'		
-		submit.style.fontSize = "8px"
-	} else {
-		submit.style.color = 'white'
-		submit.style.backgroundColor = 'black'
-		submit.style.fontSize = "6px"
-	}
-}, 2000)
-
-	var i = 0;
-setInterval(function(){
-	if(i > listNames.length){
-		i = 0;
-		changeColor(i)		
-	}else {
-		changeColor(i)
-	}
-	i += 1;
-}, 1000)
 
 
-function changeColor(i){
-	console.log(i)
-	if(i == 0){
-		listNames[i].style.color = 'red'
-	}else if(i > 0 && i < listNames.length){
-		listNames[i].style.color = 'red'
-		listNames[i-1].style.color = 'black'
-	}else if(i == listNames.length){
-		listNames[i-1].style.color='black'
-	}
-}
+
+
+
+
+
 
